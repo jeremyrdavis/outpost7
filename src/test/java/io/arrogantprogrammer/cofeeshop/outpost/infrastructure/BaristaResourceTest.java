c1d2ff48306ca60eff4f2a7fbfd42de8e3907e07
jeremyrdavis <jeremyrdavis@mac.com>
2024-04-02 08:42:25 -0400

    initial

---
+++ src/test/java/io/arrogantprogrammer/cofeeshop/outpost/infrastructure/BaristaResourceTest.java
@@ -0,0 +1,35 @@
+package io.arrogantprogrammer.cofeeshop.outpost.infrastructure;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+
+@QuarkusTest
+public class BaristaResourceTest {
+    static final Logger LOGGER = LoggerFactory.getLogger(BaristaResourceTest.class);
+
+    String json = """
+            {
+                "uuid": "123e4567-e89b-12d3-a456-426614174000",
+                "item": "COFFEE",
+                "name": "Larry"
+            }
+            """;
+
+    @Test
+    public void testMakeCoffee() {
+        LOGGER.info("Testing makeCoffee method");
+        given()
+                .body(json)
+                .when()
+                .post("/make")
+                .then()
+                .statusCode(201)
+                .body(contains("Larry"));
+    }
+}
